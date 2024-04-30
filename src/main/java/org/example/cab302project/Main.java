3e72195d35fc8fbfb17b87ea6b83c5e558d73526
Jackson Richards <41310539+Dukesan7@users.noreply.github.com>
2024-04-30 16:13:52 +1000

    each page has controller fixed page navigation

--- src/main/java/org/example/cab302project/Main.java
+++ src/main/java/org/example/cab302project/Main.java
@@ -1,41 +1,23 @@
 package org.example.cab302project;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.VBox;
-import java.io.IOException;
 
 public class Main extends Application {
-    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+        primaryStage.setTitle("JavaFX Multi-page App");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
         launch(args);
     }
-
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        goToPage("Dashboard");
-        primaryStage.setTitle("idk app name here");
-        primaryStage.show();
-    }
-
-    public void goToPage(String pageName) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(pageName + ".fxml"));
-            Parent root = loader.load();
-
-            // Set the scene with the loaded root
-            Scene scene = new Scene(root, 800, 600);
-            primaryStage.setScene(scene);
-
-            PageController controller = loader.getController();
-            controller.setMainApp(this);
-        } catch (IOException error) {
-            error.printStackTrace();
-        }
-    }
-
 }
