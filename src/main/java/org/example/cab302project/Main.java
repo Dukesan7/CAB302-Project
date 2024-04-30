4b9105dd102e3d35c7bd95b1b830c3c609c1107b
Jackson Richards <41310539+Dukesan7@users.noreply.github.com>
2024-04-30 21:29:01 +1000

    Added create database function into main

--- src/main/java/org/example/cab302project/Main.java
+++ src/main/java/org/example/cab302project/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        CreateDB.createDatabase();
         Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
         primaryStage.setTitle("JavaFX Multi-page App");
         primaryStage.setScene(new Scene(root, 600, 400));
