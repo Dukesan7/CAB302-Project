97176a6a450f68fcde9c979eff8ed9e3e3934000
Jackson Richards <41310539+Dukesan7@users.noreply.github.com>
2024-05-17 23:25:21 +1000

    added json library import

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.cab302project {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
+    requires org.json;
 
     opens org.example.cab302project to javafx.fxml;
     exports org.example.cab302project;
