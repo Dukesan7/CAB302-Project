481d97e4a666ff24e0a7a0447da711d4f4c737cb
Wierdwizard2587 <58847456+Wierdwizard2587@users.noreply.github.com>
2024-06-01 18:10:26 +1000

    created test case files

---
+++ src/main/test/initSessModel.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.cab302project.initialiseSess;
+
+public class initSessModel {
+
+
+}
