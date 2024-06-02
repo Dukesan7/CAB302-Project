631ed3e70c8dc2dbe9aa92469fefbe04551d9451
DomaldDuck <n11298057@qut.edu.au>
2024-06-02 16:10:23 +1000

    To send over to PC

--- src/main/java/org/example/cab302project/Controller/ProfilesPageController.java
+++ src/main/java/org/example/cab302project/Controller/ProfilesPageController.java
@@ -24,21 +24,6 @@ public class ProfilesPageController {
     private LoginPageController loginPage;
     private PageFunctions pageFunctions = new PageFunctions();
     private Connection connection;
-    public ArrayList<String> getProfileName() {
-        return profileName;
-    }
-    public void setProfileName(ArrayList<String> profileName) { this.profileName = profileName; }
-    private ArrayList<String> profileName = new ArrayList<>();
-    private String password;
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    private ArrayList<String> smName = new ArrayList<>();
-    private int smIndex;
     private String selectedQuestion;
     ObservableList<String> potentialQuestions = FXCollections.observableArrayList(
             "What is the name of your first pet?",
@@ -47,50 +32,6 @@ public class ProfilesPageController {
             "What is your favourite ice cream flavour?"
     );
 
-//    public ProfilesPageController(String profileName, String password, String smName, String selectedQuestion ) {
-//        this.profileName.add(profileName);
-//        this.password = password;
-//        this.smName.add(smName);
-//        this.selectedQuestion = selectedQuestion;
-//    }
-
-
-    // ~*~*~*~*~*~*~*~ DATABASE STUFF ~*~*~*~*~*~*~*~
-    private static final String DB_FILE_PATH = "src/main/resources/org/example/cab302project/ToDo.db";
-    private Connection connect() throws SQLException {
-        return DriverManager.getConnection("jdbc:sqlite:" + DB_FILE_PATH);
-    }
-
-    public void AddNewProfile(String newProfileName) {
-        this.profileName.add(newProfileName);
-    }
-
-    public String ChangePassword(String oldPw, String newPw, String confirmNewPw) {
-        if (oldPw.equals(newPw)) { return "Please enter a different password"; }
-        if (!newPw.equals(confirmNewPw)) {return "Ensure the confirmed password is correct"; }
-
-        //change shit in db();
-        return "Password has successfully been changed!";
-    }
-
-    public String AppendStudyMode(String nameofMode) {
-        int count = smName.size();
-
-        //checks for duplicates
-        for (String s : smName) {
-            if (s.equals(nameofMode)) {
-                return "Please try a different name for the study mode."; }
-        }
-        smName.add(nameofMode);
-        // checks to see if string is successfully added
-        if (smName.size() <= count) {
-            return "The Study Mode was unable to be added, please try again."; }
-
-        return MessageFormat.format("Study Mode {0} Successfully added.", smName.get(count));
-    }
-
-    @FXML
-    TextField addStudyModeInput;
     @FXML
     ComboBox<String> changeSecurityQuestion;
     @FXML
@@ -100,13 +41,6 @@ public class ProfilesPageController {
     @FXML
     HBox hBox;
 
-
-    @FXML
-    private void returnTextAndAppend() {
-        String addedStudyModeName = addStudyModeInput.getText();
-        String returnString = AppendStudyMode(addedStudyModeName);
-        System.out.println(returnString);
-    }
     @FXML
     private void populateSecurityQuestions() {
         String questionPreview;
@@ -131,15 +65,6 @@ public class ProfilesPageController {
     private void populateProfileDisplayName() {
         try {profileDisplayName.setText("Current Profile: " + LoginPageController.nameOfUser); }
         catch (NullPointerException e) {System.err.println(e.getMessage());}
-    }
-
-    @FXML
-    public void changeButtonLabel(ActionEvent event) {
-        ToggleButton tButton = (ToggleButton) event.getSource();
-        if (tButton.getText().equals("OFF")) {
-            tButton.setText("ON");
-        }
-        else { tButton.setText("OFF");}
     }
 
     @FXML
@@ -179,8 +104,7 @@ public class ProfilesPageController {
         String hashAnswer = hashString(answer);
 
         String sql = "UPDATE UserDetails SET securityQuestion = ?, securityAnswer = ? WHERE UserID = ?";
-        try (Connection conn = connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
 
             int userID = loginPage.userID;
 
